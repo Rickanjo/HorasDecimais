55aae8f8b0987cab16a59aeebc7f933692149b0b
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-22 13:01:01 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("---- Horas Decimais ----\n");
+
+Console.Write("Digite um valor decimal de horas: ");
+double horas = double.Parse(Console.ReadLine()!);
+
+int horasInt = (int)horas;
+double minutosRestantes = (horas - horasInt) * 60;
+int minutosInt = (int)minutosRestantes;
+int segundosRestantes = (int)((minutosRestantes - minutosInt) * 60);
+
+Console.WriteLine($"\n{horasInt} horas, {minutosInt} minutos e {segundosRestantes} segundos.");
